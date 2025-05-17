c695e2bfe58152a6a2e6682eae832511d89571db
Ryan Faircloth <ryan@dss-i.com>
2025-05-17 06:54:56 -0700

    chore: replace license

--- tests/test_decoders/plugins/message/generic/cef/test_cef_integration.py
+++ tests/test_decoders/plugins/message/generic/cef/test_cef_integration.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2025 ziggiz
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# # SPDX-License-Identifier: BSL-1.1
+# # Copyright (c) 2025 Ziggiz Inc.
+# #
+# # This file is part of the ziggiz-courier-core-data-processing and is licensed under the
+# # Business Source License 1.1. You may not use this file except in
+# # compliance with the License. You may obtain a copy of the License at:
+# # https://github.com/ziggiz-courier/ziggiz-courier-core-data-processing/blob/main/LICENSE
 """
 Integration tests for GenericCEFDecoderPlugin using manual processing.
 These tests verify that the CEF decoder plugin works correctly when processing
